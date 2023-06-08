d9ef395766820996bbe9bf30344e7610b848c47c
Satara <tatenokay@gmail.com>
2023-06-08 16:35:04 -0500

    i18nLang

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
-
 import HomePage from './pages/home'
-import Abaut from './pages/abaut'
 import { Router } from './pages/Router'
 import Page404 from './pages/404'
 import SearchPage from './pages/Search'
 import { Route } from './Route'
+import { Suspense, lazy } from 'react'
+
+const LazyAbaut = lazy(() => import('./pages/abaut'))
 
 const routes = [
+  {
+    path: '/:lang/abaut',
+    Component: LazyAbaut
+  },
   {
     path:'/search/:query',
     Component: SearchPage
@@ -17,11 +22,14 @@ const routes = [
 function App() {
   return (
     <main>
+      <Suspense fallback={<div>Loading...</div>}>
       <Router routes={routes} defauldComponent={Page404}>
         <Route path='/' Component={HomePage} />
-        <Route path='/abaut' Component={Abaut}/>
+        <Route path='/abaut' Component={LazyAbaut}/>
       </Router>
+      </Suspense>
     </main>
+    
   )
 }
 
--- src/pages/abaut.jsx
+++ src/pages/abaut.jsx
@@ -1,13 +1,31 @@
 import {Link} from '../Link'
 
-export default function Abaut(){
+const i18n = {
+  es:{
+    title: 'sobre nosotros',
+    button: 'Ir a Home',
+    descripcion: 'ejemplos de react router'
+  },
+  en:{
+    title: 'About us',
+    button: 'Go to Home',
+    descripcion: 'texto en ingles'
+  }
+}
+
+const useI18n = (lang) => {
+  return i18n[lang] || i18n.en
+}
+
+export default function Abaut({routerParams}){
+  const i18n = useI18n(routerParams.lang ?? 'es')
   return(
     <>
-      <h1>Abaut</h1>
+      <h1>{i18n.title}</h1>
       <img src="https://avatars.githubusercontent.com/u/25549072?v=4" alt="" />
-      <p>Pagina de Abaut</p>
+      <p>{i18n.descripcion}</p>
       {/* <button onClick={() => navigate("/")}>Ir a la Home</button> */}
-      <Link to='/' >Ir Home</Link>
+      <Link to='/' >{i18n.button}</Link>
     </>
   )
 }
